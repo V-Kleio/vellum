32e73a26b32e9b6885df6adc3588b8d1bdc1c5de
Vylon-Kleio <rafaelmarchel.dw@gmail.com>
2025-07-02 23:16:26 +0700

    ci: remove security scanning to simplify pipeline
    - Remove OWASP dependency check (API key issues)
    - Add GitHub Dependabot for dependency security

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     `java-library`
     `maven-publish`
     checkstyle
-    id("org.owasp.dependencycheck") version "9.2.0" apply false
 }
 
 allprojects {
@@ -17,9 +16,7 @@ subprojects {
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
     apply(plugin = "checkstyle")
-    apply(plugin = "org.owasp.dependencycheck")
 
-    // Java configuration
     java {
         toolchain {
             languageVersion = JavaLanguageVersion.of(21)
@@ -28,18 +25,11 @@ subprojects {
         withJavadocJar()
     }
 
-    // Checkstyle configuration
     checkstyle {
         toolVersion = "10.12.4"
         configFile = rootProject.file("config/checkstyle/checkstyle.xml")
         isIgnoreFailures = false
         maxWarnings = 0
-    }
-
-    // Dependency Check configuration
-    configure<org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension> {
-        formats = listOf("HTML")
-        suppressionFile = rootProject.file("config/dependency-check-suppressions.xml").path
     }
 
     dependencies {
