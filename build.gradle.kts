e87503e49bd662bba262e64e2762997810c310f8
Vylon-Kleio <rafaelmarchel.dw@gmail.com>
2025-07-02 21:54:14 +0700

    chore: root build apply plugins conditionally

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
-    `java-library`
-    `maven-publish`
+    `java-library` apply false
+    `maven-publish` apply false
+    id("checkstyle") apply false
+    id("org.owasp.dependencycheck") version "9.2.0" apply false
 }
 
 allprojects {
     group = "io.vellum"
-    version = "0.1.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
@@ -15,7 +16,9 @@ allprojects {
 subprojects {
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
-    
+    apply(plugin = "checkstyle")
+    apply(plugin = "org.owasp.dependencycheck")
+
     java {
         toolchain {
             languageVersion = JavaLanguageVersion.of(21)
@@ -23,9 +26,22 @@ subprojects {
         withSourcesJar()
         withJavadocJar()
     }
-    
+
+    checkstyle {
+        toolVersion = "10.12.4"
+        configFile = rootProject.file("config/checkstyle/checkstyle.xml")
+        isIgnoreFailures = false
+        maxWarnings = 0
+    }
+
+    dependencyCheck {
+        format = "HTML"
+        suppressionFile = rootProject.file("config/dependency-check-suppressions.xml").path
+    }
+
     dependencies {
-        testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+        testImplementation(platform("org.junit:junit-bom:5.10.2"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
     
