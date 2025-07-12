a49df1f1ae305afea839d2ff9a3d157189de6b80
Vylon-Kleio <rafaelmarchel.dw@gmail.com>
2025-07-12 21:31:21 +0700

    feat: implement Sketch class

---
+++ vellum-core/src/main/java/io/vellum/core/Sketch.java
@@ -0,0 +1,11 @@
+package io.vellum.core;
+
+import io.vellum.core.renderer.Renderer;
+
+public class Sketch {
+    private final Renderer renderEngine;
+
+    public Sketch(Renderer renderEngine) {
+        this.renderEngine = renderEngine;
+    }
+}
